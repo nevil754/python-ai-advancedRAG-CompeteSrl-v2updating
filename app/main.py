f26838bc7a79d637d504f29b6c739d12159449ab
Nelvison Benedetto <nelvisonb@compet-e.it>
2026-05-27 10:43:17 +0200

    update

--- app/main.py
+++ app/main.py
@@ -1,2 +1,16 @@
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from loguru import logger
+
+from app.core.observability import setup_all
+from app.core.settings import get_settings
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+from __future__ import annotations
+
+settings = get_settings()
+
+
+
 
